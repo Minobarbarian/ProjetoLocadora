70f56d8f77c8bccced308926addadf0976a3e2ee
Jacpc <tonho_kun@ymail.com>
2024-05-31 14:00:32 -0300

    Add chaveamento estrangeiro

--- ProjetoLocadora/src/br/ufrn/imd/modelo/Carro.java
+++ ProjetoLocadora/src/br/ufrn/imd/modelo/Carro.java
@@ -2,17 +2,24 @@ package br.ufrn.imd.modelo;
 
 public class Carro extends Veiculo{
 	private int id_carro;
+	private int id_veiculo;
 	private int portas;
 	private int potencia;
 	
 	public int getId_carro() {
 		return id_carro;
 	}
+	public int getId_veiculo() {
+		return id_veiculo;
+	}
 	public int getPortas() {
 		return portas;
 	}
 	public int getPotencia() {
 		return potencia;
+	}
+	public void setId_veiculo(int id_veiculo) {
+		this.id_veiculo = id_veiculo;
 	}
 	public void setId_carro(int id_carro) {
 		this.id_carro = id_carro;
--- ProjetoLocadora/src/br/ufrn/imd/modelo/Moto.java
+++ ProjetoLocadora/src/br/ufrn/imd/modelo/Moto.java
@@ -2,10 +2,15 @@ package br.ufrn.imd.modelo;
 
 public class Moto extends Veiculo{
 	private int id_moto;
+	private int id_veiculo;
 	private int cilindradas;
 	private String partida;
+	
 	public int getId_moto() {
 		return id_moto;
+	}
+	public int getId_veiculo() {
+		return id_veiculo;
 	}
 	public int getCilindradas() {
 		return cilindradas;
@@ -15,6 +20,9 @@ public class Moto extends Veiculo{
 	}
 	public void setId_moto(int id_moto) {
 		this.id_moto = id_moto;
+	}
+	public void setId_veiculo(int id_veiculo) {
+		this.id_veiculo = id_veiculo;
 	}
 	public void setCilindradas(int cilindradas) {
 		this.cilindradas = cilindradas;
