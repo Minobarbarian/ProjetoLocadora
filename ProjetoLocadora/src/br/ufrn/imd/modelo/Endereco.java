cd1563696d299d70024ef2ef229fe797710150e2
Jacpc <tonho_kun@ymail.com>
2024-05-31 14:07:17 -0300

    add id

--- ProjetoLocadora/src/br/ufrn/imd/modelo/Endereco.java
+++ ProjetoLocadora/src/br/ufrn/imd/modelo/Endereco.java
@@ -1,11 +1,15 @@
 package br.ufrn.imd.modelo;
 
 public class Endereco {
+	protected int id_endereco;
 	protected String rua;
 	protected String cidade;
 	protected String estado;
 	protected String cep;
 	
+	public int getId_endereco() {
+		return id_endereco;
+	}
 	public String getRua() {
 		return rua;
 	}
@@ -17,6 +21,9 @@ public class Endereco {
 	}
 	public String getCep() {
 		return cep;
+	}
+	public void setId_endereco(int id_endereco) {
+		this.id_endereco = id_endereco;
 	}
 	public void setRua(String rua) {
 		this.rua = rua;
@@ -30,6 +37,5 @@ public class Endereco {
 	public void setCep(String cep) {
 		this.cep = cep;
 	}
-	
 	
 }
