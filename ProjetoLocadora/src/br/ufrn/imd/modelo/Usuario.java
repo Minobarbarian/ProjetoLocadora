f0cfe5110d091f92868a9a565ef0e5e8aa77ac4d
Jacpc <tonho_kun@ymail.com>
2024-05-31 13:50:06 -0300

    Chaveamento estrangeiro para endereço

--- ProjetoLocadora/src/br/ufrn/imd/modelo/Usuario.java
+++ ProjetoLocadora/src/br/ufrn/imd/modelo/Usuario.java
@@ -4,16 +4,19 @@ import java.time.LocalDate;
 
 public class Usuario {
 	protected int id_usuario;
+	protected int id_endereco;
 	protected String nome;
 	protected String login;
 	protected String senha;
 	protected String email;
 	protected String telefone;
 	protected LocalDate dataNascimento;
-	protected Endereco endereco;
 	
 	public int getId_usuario() {
 		return id_usuario;
+	}
+	public int getId_endereco() {
+		return id_endereco;
 	}
 	public String getNome() {
 		return nome;
@@ -33,11 +36,11 @@ public class Usuario {
 	public LocalDate getDataNascimento() {
 		return dataNascimento;
 	}
-	public Endereco getEndereco() {
-		return endereco;
-	}
 	public void setId_usuario(int id_usuario) {
 		this.id_usuario = id_usuario;
+	}
+	public void setId_endereco(int id_endereco) {
+		this.id_endereco = id_endereco;
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
@@ -57,9 +60,5 @@ public class Usuario {
 	public void setDataNascimento(LocalDate dataNascimento) {
 		this.dataNascimento = dataNascimento;
 	}
-	public void setEndereco(Endereco endereco) {
-		this.endereco = endereco;
-	}
-	
 	
 }
