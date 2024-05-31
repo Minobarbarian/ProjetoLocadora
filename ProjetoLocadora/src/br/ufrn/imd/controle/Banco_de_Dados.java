ed77ec9927caba748240a0cc643287a11584f807
Jacpc <tonho_kun@ymail.com>
2024-05-31 13:34:57 -0300

    Mudando nome do bd

--- ProjetoLocadora/src/br/ufrn/imd/controle/Banco_de_Dados.java
+++ ProjetoLocadora/src/br/ufrn/imd/controle/Banco_de_Dados.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Banco_de_Dados {
-	private static final String URL = "jdbc:mysql://localhost:3306/locadora";
+	private static final String URL = "jdbc:mysql://localhost:3306/Locadora";
 	private static final String NOME = "root";
 	private static final String SENHA = "root";
 	
