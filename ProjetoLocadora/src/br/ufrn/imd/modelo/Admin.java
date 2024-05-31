0829184ab2b7f2054425cb7f5239468772b39ce1
Jacpc <tonho_kun@ymail.com>
2024-05-31 13:55:49 -0300

    Chaveamento estrangeiro para usuario

--- ProjetoLocadora/src/br/ufrn/imd/modelo/Admin.java
+++ ProjetoLocadora/src/br/ufrn/imd/modelo/Admin.java
@@ -2,12 +2,16 @@ package br.ufrn.imd.modelo;
 
 public class Admin extends Usuario{
 	private int id_admin;
+	private int id_usuario;
 	private String cargo;
 	private String supervisor;
 	private String departamento;
 	
 	public int getId_admin() {
 		return id_admin;
+	}
+	public int getId_usuario() {
+		return id_usuario;
 	}
 	public String getCargo() {
 		return cargo;
@@ -20,6 +24,9 @@ public class Admin extends Usuario{
 	}
 	public void setId_admin(int id_admin) {
 		this.id_admin = id_admin;
+	}
+	public void setId_usuario(int id_usuario) {
+		this.id_usuario = id_usuario;
 	}
 	public void setCargo(String cargo) {
 		this.cargo = cargo;
